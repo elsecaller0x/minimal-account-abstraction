b115010e078515fb2814d1703dc24a80d7d30ab4
elsecaller0x <wierdcreature0x@gmail.com>
2025-08-10 21:08:07 +0100

    Update HelperConfig.s.sol
    minor change from 42_161 to 42161

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -29,7 +29,7 @@ contract HelperConfig is Script {
     uint256 constant LOCAL_CHAIN_ID = 31337;
     // Update the BURNER_WALLET to your burner wallet!
     address constant BURNER_WALLET = 0x643315C9Be056cDEA171F4e7b2222a4ddaB9F88D;
-    uint256 constant ARBITRUM_MAINNET_CHAIN_ID = 42_161;
+    uint256 constant ARBITRUM_MAINNET_CHAIN_ID = 42161;
     uint256 constant ZKSYNC_MAINNET_CHAIN_ID = 324;
     // address constant FOUNDRY_DEFAULT_WALLET = 0x1804c8AB1F12E6bbf3894d4083f33e07309d1f38;
     address constant ANVIL_DEFAULT_ACCOUNT = 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266;
